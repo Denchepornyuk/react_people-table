eb33f96b4684a090be6e9801ccbf092c524ea90c
Denys <denys.cheporniuk@gmail.com>
2020-01-02 15:25:17 +0200

    Updates

---
+++ precache-manifest.c5a0fbd541dbdac38f6155032229c65d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67cce4557b357f72e76d663153fc6ac",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "505bb7756453afb7c016",
+    "url": "/react_people-table/static/css/main.754a7c50.chunk.css"
+  },
+  {
+    "revision": "8b7ba9a5e56ea38a666a",
+    "url": "/react_people-table/static/js/2.ce3ed489.chunk.js"
+  },
+  {
+    "revision": "505bb7756453afb7c016",
+    "url": "/react_people-table/static/js/main.6ab398f9.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
